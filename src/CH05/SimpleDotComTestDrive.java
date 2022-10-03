1ad788bc54a0c5153dd87dbcb1d6fbd4525bedf3
Armi Ipapo <amiinternetofthings@gmail.com>
2022-10-03 17:29:26 +0800

    new classes ch05

---
+++ src/CH05/SimpleDotComTestDrive.java
@@ -0,0 +1,13 @@
+package CH05;
+
+public class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+        SimpleDotCom dot = new SimpleDotCom();
+        int[] locations = {2, 3, 4};
+        dot.setLocationCells(locations);
+        String userGuess = "8";
+        String result = dot.checkYourself(userGuess);
+
+    }
+
+}
