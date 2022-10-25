79ea7bc766e049baf4af248ba29d58b52423aaba
Armi Ipapo <amiinternetofthings@gmail.com>
2022-10-25 10:58:02 +0800

    new classes ch05

--- src/amireading/Read.java
+++ src/amireading/Read.java
@@ -3,6 +3,10 @@ package amireading;
 public class Read {
     void startRead(Meter a) {
         int result = a.getMeterCount();
-        System.out.println("Meter Reads: "  +result);
+
+            System.out.println("MEter Reads: "+a.getClass()+ " "   +result);
+
     }
-}
+
+    }
+
