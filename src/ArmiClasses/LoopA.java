535574d21aa895e8a233daa8d621b715bbc3c129
Armi Ipapo <amiinternetofthings@gmail.com>
2022-10-28 10:25:49 +0800

    new classes ch05

--- src/ArmiClasses/LoopA.java
+++ src/ArmiClasses/LoopA.java
@@ -2,8 +2,8 @@ package ArmiClasses;
 
 public class LoopA {
     public static void main(String[] args){
-        int x;
-        for (x=0; x<9; x++) {
+        //int x;
+        for (int x=0; x<20; x++) {
             for (int b=1; b<=x; b++) {
                 System.out.print(b +" ");
             }
