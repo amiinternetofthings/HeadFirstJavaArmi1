5746a829aa028fa8d9d5c83af03e026b992d908f
armi <amiinternetofthing@gmail.com>
2022-11-04 23:37:24 +0800

    armi ch5

--- src/CH09A/Duck.java
+++ src/CH09A/Duck.java
@@ -1,13 +1,27 @@
 package CH09A;
 
 public class Duck {
-    public Duck() {
+
+    int size;
+    private String name;
+    public Duck(int duckSize) {
+
         System.out.println("Quack");
+        size = duckSize;
+        System.out.println("size is " + size +" " +name);
     }
+
+    public void setName(String duckName) {
+
+        name = duckName;
+    }
+
 }
 
 class UseADuck{
     public static void main (String[] args) {
-        Duck d = new Duck();
+
+        Duck d = new Duck(42);
+        d.setName("DoooooDuck");
     }
 }
