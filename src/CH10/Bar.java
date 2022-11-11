d36872091999a933060389fda054cc513d0e4db9
Armi Ipapo <amiinternetofthings@gmail.com>
2022-11-11 15:57:28 +0800

    new classes ch05

--- src/CH10/Bar.java
+++ src/CH10/Bar.java
@@ -1,14 +1,27 @@
 package CH10;
 
 public class Bar {
+    int x = Math.abs(-600);
+    double d = Math.abs(233.43);
+
+    float c = (float) d;
+
+    int e = Math.round(c);
     public static final int BAR_X;
 
     static {
         BAR_X = (int) (Math.random() * 100);
     }
     public static void main (String[] args) {
-        System.out.println(Bar.BAR_X);
+        Bar bar = new Bar();
+        System.out.println(Bar.BAR_X+ " " + bar.x +" " +bar.d +" "+ bar.e);
     }
+
+    int i = 288;
+    Integer iWrap = Integer.valueOf(i);
+
+    int unWrapped = iWrap.intValue();
+
 
 }
 
